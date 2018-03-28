37cc5ec43a92da5e815c8eb43cd35480f4fc083e
Devin Mork <devin.mork@gmail.com>
2018-03-28 06:09:54 -0500

    Board & BoardSpace classes.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,21 @@
+# The play surface of the game. Its @root_space
+# is used to access other BoardSpace nodes across
+# the play surface.
 class Board
+  attr_reader :root_space
 
+  def initialize
+    @root_space = BoardSpace.new([0, 0])
+  end
+end
+
+# Each individual space on the board. A BoardSpace
+# node knows its own coordinates, which piece has
+# landed in it, and its relation to other nodes.
+class BoardSpace
+  attr_reader :coordinates
+
+  def initialize(coordinates)
+    @coordinates = coordinates
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,13 +2,22 @@ require 'rspec'
 
 require_relative '../lib/board.rb'
 
-describe Board
+describe Board do
   context '.initialize' do
-    before do
-      @board = Board.new
-    end
+    before { @board = Board.new }
     it 'should have the following parameters' do
-
+      expect(@board.root_space.class.to_s).to eq('BoardSpace')
+      expect(@board.root_space.coordinates).to eq([0, 0])
     end
   end
+end
+
+describe BoardSpace do
+  context '.initialize' do
+    before { @space = BoardSpace.new([0, 0]) }
+    it 'should initialize with the following parameters' do
+      expect(@space.coordinates.class.to_s).to eq('Array')
+    end
+  end
+
 end
