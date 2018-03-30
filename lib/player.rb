2c864244a0233599f5ae84eaf9fffe66a5dd2e38
Devin Mork <devin.mork@gmail.com>
2018-03-30 07:53:25 -0500

    Testing Player.show_player_count failing.

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,21 @@
+# Each Player has a name, a piece, and a wins counter.
+# Class knows to give first player a certain
+# piece and a second player a different kind of
+# piece to usein the game.
 class Player
+  attr_reader :piece
+
+  @@player_count = 0
+
+  def initialize
+    @@player_count += 1
+    @piece = '⚪' if @@player_count == 1
+    @piece = "\u26AB".encode('utf-8') if @@player_count == 2
+  end
+
+  def show_player_count
+    @@player_count
+  end
+
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,16 @@
 require 'rspec'
 
-require_relative '../lib/player'
+require_relative '../lib/player.rb'
 
-describe Player do  
+describe Player do
+
+  describe '.initialize' do
+    @player_one = Player.new
+    @player_two = Player.new
+    puts @player_one.show_player_count
+
+    it { expect(@player_one.show_player_count).to eq(2) }
+  end
+
 
 end
