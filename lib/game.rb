511d443c6a0d0595de6cf38e4739864d9c087c3f
Devin Mork <devin.mork@gmail.com>
2018-03-31 07:26:03 -0500

    Try to stub inputer on gamespec.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,6 @@
+require_relative 'player.rb'
+require_relative 'board.rb'
+
 class Game
 
   attr_reader :player_1, :player_2, :board
@@ -8,4 +11,18 @@ class Game
     @board = board
   end
 
+  def get_player_names
+
+    
+  end
+
+  def number_of_players
+    puts 'How many people would like to play?'
+    number = gets.chomp.to_i
+    puts 'Human vs. Computer' if number == 1
+    # return number
+  end
+
 end
+
+connect_four = Game.new(Player.new, Player.new, Board.new)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,7 @@ require_relative '../lib/player.rb'
 require_relative '../lib/board.rb'
 
 describe Game do
-  context '.initialize' do
+  describe '.initialize' do
     before do
       @game = Game.new(Player.new, Player.new, Board.new)
     end
@@ -14,6 +14,23 @@ describe Game do
       expect(@game.player_1.class.to_s).to eq('Player')
       expect(@game.player_2.class.to_s).to eq('Player')
       expect(@game.board.class.to_s).to eq('Board')
+    end
+  end
+
+  describe '#get_player_names' do
+    game = Game.new(Player.new, Player.new, Board.new)
+    it { expect(game).to respond_to(:get_player_names) }
+  end
+
+  describe '#number_of_players' do
+    game = Game.new(Player.new, Player.new, Board.new)
+    it { expect(game).to respond_to(:number_of_players) }
+    it "confirms Human vs. Computer" do
+      allow(STDIN).to receive("1\n")
+      expect(STDOUT).to receive(:puts).with('How many people would like to play?')
+      expect(STDOUT).to receive(:puts).with('Human vs. Computer')
+      
+      game.number_of_players
     end
   end
 end
