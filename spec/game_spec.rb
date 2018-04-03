35caf93c9beaa3cf4b5fb14db78066f53b131b33
Devin Mork <devin.mork@gmail.com>
2018-04-02 20:34:17 -0500

    Testing  &  successfully in game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,30 +5,26 @@ require_relative '../lib/player.rb'
 require_relative '../lib/board.rb'
 
 describe Game do
+  let(:game) { Game.new(Player.new, Player.new, Board.new) }
+
   describe '.initialize' do
-    before do
-      @game = Game.new(Player.new, Player.new, Board.new)
-    end
+    
 
     it 'has two players and a board' do
-      expect(@game.player_1.class.to_s).to eq('Player')
-      expect(@game.player_2.class.to_s).to eq('Player')
-      expect(@game.board.class.to_s).to eq('Board')
+      expect(game.player_1.class.to_s).to eq('Player')
+      expect(game.player_2.class.to_s).to eq('Player')
+      expect(game.board.class.to_s).to eq('Board')
     end
   end
 
   describe '#get_player_names' do
-    game = Game.new(Player.new, Player.new, Board.new)
     it { expect(game).to respond_to(:get_player_names) }
   end
 
   describe '#number_of_players' do
-    @input = StringIO.new('1')
-    @output = StringIO.new
-    game = Game.new(Player.new, Player.new, Board.new)
-    it { expect(game).to respond_to(:number_of_players) }
-
-    it { expect(game.number_of_players(@input, @output)).to eq(@input) }
-    it { expect(@output.read).to eq('How many people would like to play?') }
+    it 'can choose one player' do
+      allow(game).to receive(:gets).and_return('1')
+      expect { expect(game.number_of_players).to eq(1) }.to output("How many people would like to play?\nHuman vs. Computer\n").to_stdout
+    end
   end
 end
